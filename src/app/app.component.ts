bd57d4494ebb15ac1c9ec74542a61201e99cffa2
Hiroyuki Shigeta <shigeta@glgl.org>
2017-03-09 10:42:33 +0900

    add nested form sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  items: Array<any> = [];
+
+  ngOnInit(): void {
+    this.items = [{name: 'a'}, {name: 'b'}, {name: 'c'}];
+  }
+
+  addItem() {
+    this.items.push({name: ''});
+  }
+
+  removeItem(i) {
+    this.items.splice(i, 1);
+  }
 }
